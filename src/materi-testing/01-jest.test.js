2c88419a0c8f65b0c5a589e496c6d8347925fd04
Dwipayana <dwipayanaismulya6@gmail.com>
2024-06-19 22:40:09 +0800

    first commit

---
+++ src/materi-testing/01-jest.test.js
@@ -0,0 +1,23 @@
+function penjumlahan(angka1, angka2) {
+  return angka1 + angka2;
+}
+
+function perkalian(angka1, angka2) {
+  return angka1 * angka2;
+}
+
+test("testing sederhana", () => {
+  expect(true).toBe(true);
+});
+
+test("menjumlahkan nilai", () => {
+  expect(1 + 1).toBe(2);
+});
+test("Fungsi penjumlahan", () => {
+  expect(penjumlahan(3, 2)).toBe(5);
+  expect(penjumlahan(4, 8)).toBe(12);
+});
+test("Fungsi perkalian", () => {
+  expect(perkalian(3, 2)).toBe(6);
+  expect(perkalian(5, 3)).toBe(15);
+});
